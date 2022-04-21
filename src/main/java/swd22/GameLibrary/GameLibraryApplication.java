03276fea919a698b22b85f2d037dbf792ab9f327
vivkiri <elina.elkama@hotmail.com>
2022-04-21 12:49:50 +0300

    commandlinerunner added commands

--- src/main/java/swd22/GameLibrary/GameLibraryApplication.java
+++ src/main/java/swd22/GameLibrary/GameLibraryApplication.java
@@ -45,29 +45,71 @@ public class GameLibraryApplication {
 			Attribute attribute2 = new Attribute("Co-op");
 			Attribute attribute3 = new Attribute("Role-Playing");
 			Attribute attribute4 = new Attribute("First-Person");
-			Attribute attribute5 = new Attribute("Player-versus-Player");
+			Attribute attribute5 = new Attribute("Fantasy");
+			Attribute attribute6 = new Attribute("Sci-fi");
+			Attribute attribute7 = new Attribute("Open World");
+			Attribute attribute8 = new Attribute("Action");
+			Attribute attribute9 = new Attribute("Adventure");
+			Attribute attribute10 = new Attribute("Multiplayer");
 			
 			attributeRepository.save(attribute1);
 			attributeRepository.save(attribute2);
 			attributeRepository.save(attribute3);
 			attributeRepository.save(attribute4);
 			attributeRepository.save(attribute5);
+			attributeRepository.save(attribute6);
+			attributeRepository.save(attribute7);
+			attributeRepository.save(attribute8);
+			attributeRepository.save(attribute9);
+			attributeRepository.save(attribute10);
 			
 			List<Attribute> game1Attributes = new ArrayList<>();
 			game1Attributes.add(attribute2);
 			game1Attributes.add(attribute5);
+			
+			List<Attribute> game4Attributes = new ArrayList<>();
+			game1Attributes.add(attribute6);
+			game1Attributes.add(attribute7);
+			game1Attributes.add(attribute1);
+			game1Attributes.add(attribute8);
+			game1Attributes.add(attribute4);
+			
+			List<Attribute> game6Attributes = new ArrayList<>();
+			game1Attributes.add(attribute7);
+			game1Attributes.add(attribute9);
+			game1Attributes.add(attribute5);
+			game1Attributes.add(attribute1);
+			game1Attributes.add(attribute3);
+			
+			List<Attribute> game5Attributes = new ArrayList<>();
+			game1Attributes.add(attribute7);
+			game1Attributes.add(attribute1);
+			game1Attributes.add(attribute10);
+			game1Attributes.add(attribute8);
+			
+			List<Attribute> game7Attributes = new ArrayList<>();
+			game1Attributes.add(attribute1);
+			game1Attributes.add(attribute3);
+			game1Attributes.add(attribute5);
+			game1Attributes.add(attribute8);
 
 			Game game1 = new Game("Dead By Daylight", 2016, 18, platform1, game1Attributes);
 			Game game2 = new Game("Fall Guys: Ultimate Knockout", 2020, 3, platform1);
 			Game game3 = new Game("Phasmophobia", 2020, 0, platform1);
-			Game game4 = new Game("Cyberpunk 2077", 2020, 18, platform3);
-			Game game5 = new Game("Read Dead Redemption 2", 2019, 18, platform4);
+			Game game4 = new Game("Cyberpunk 2077", 2020, 18, platform2, game4Attributes);
+			Game game5 = new Game("Read Dead Redemption 2", 2019, 18, platform3, game5Attributes);
+			Game game6 = new Game("The Elder Scrolls IV: Oblivion", 2006, 16, platform1, game6Attributes);
+			Game game7 = new Game("Jade Empire", 2007, 16, platform4, game7Attributes);
+			Game game8 = new Game("Assassin's Creed", 2007, 18, platform5);
 			
 			gameRepository.save(game1);
 			gameRepository.save(game2);
 			gameRepository.save(game3);
 			gameRepository.save(game4);
 			gameRepository.save(game5);
+			gameRepository.save(game6);
+			gameRepository.save(game7);
+			gameRepository.save(game8);
 			
 			User user1 = new User("elina", "$2a$10$3TqmIoCss2ezHiUifteKf.tMbMtpUFlU9lCErNbVSrm94Gl5Z..Da", "ADMIN");
 			User user2 = new User("teacher", "$2a$10$WiIjuE8c4D/T2XGIhjWA.OGaLzjR3kAAQ5X1YAeAsjp16Yh4U0JXy", "ADMIN");
